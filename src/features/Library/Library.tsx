73e06f4ab133dd8773fd4c5bea453905edf880a0
Arthur Silva <arthurmatsilva@outlook.com>
2021-12-17 16:48:06 -0300

    feat: FilterBar component

--- src/features/Library/Library.tsx
+++ src/features/Library/Library.tsx
@@ -1,3 +1,11 @@
+import { Box } from '@material-ui/core';
+
+import { FilterBar } from './components/FilterBar';
+
 export const Library = () => (
-  <h1>Hello, World!</h1>
+  <Box>
+    <h1>{process.env.REACT_APP_APP_NAME}</h1>
+
+    <FilterBar />
+  </Box>
 );
---
+++ src/features/Library/components/FilterBar/FilterBar.tsx
@@ -0,0 +1,10 @@
+import { Box, TextField, Button } from '@material-ui/core';
+
+export const FilterBar = () => {
+  return (
+    <Box>
+      <TextField />
+      <Button>Buscar</Button>
+    </Box>
+  );
+}
