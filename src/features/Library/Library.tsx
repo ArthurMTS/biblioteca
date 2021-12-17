c1d902bc6555ce46ca5bc9909fb10772be6315a0
Arthur Silva <arthurmatsilva@outlook.com>
2021-12-17 17:16:20 -0300

    feat: api config

--- src/features/Library/Library.tsx
+++ src/features/Library/Library.tsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from 'react';
 import { Box } from '@material-ui/core';
 
 import { FilterBar } from './components/FilterBar';
 
-export const Library = () => (
-  <Box>
-    <h1>{process.env.REACT_APP_APP_NAME}</h1>
+import { api } from '../../_config/api';
 
-    <FilterBar />
-  </Box>
-);
+type Book = {
+  title: string;
+  created_at: string;
+}
+
+export const Library = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const handleRequest = async () => {
+      const result = await api.get('search');
+
+      const { hits } = result.data;
+
+      setBooks(hits);
+    }
+
+    handleRequest();
+  }, []);
+
+  return (
+    <Box>
+      <h1>{process.env.REACT_APP_APP_NAME}</h1>
+
+      <FilterBar />
+
+      <Box>
+        {
+          books.map(book => <p key={book.created_at}>{book.title}</p>)
+        }
+      </Box>
+    </Box>
+  );
+};
